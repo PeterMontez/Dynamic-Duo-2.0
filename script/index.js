42b12800cce3ced557803b9002d4139e45a03850
Nycollas <sobolevski.academy@gmail.com>
2023-01-24 11:30:35 -0300

    ADD: first page, css and script

---
+++ script/index.js
@@ -0,0 +1,18 @@
+function ViewDisplay(id) {
+    document.getElementById(id).style.display = 'flex';
+    document.getElementById('Main').classList.add('Blur')
+    console.log('slalslasla');
+}
+
+function ClearDisplay() {
+    let devolver = document.getElementById('Devolver')
+    let retirar = document.getElementById("Retirar")
+    if (devolver.style.display != 'flex' || retirar.style.display != 'flex') {
+        devolver.style.display = 'none'
+        retirar.style.display = 'none'
+        console.log('entrou');
+    }
+    console.log('sla');
+    document.getElementById('Main').classList.remove('Blur')
+}
+
