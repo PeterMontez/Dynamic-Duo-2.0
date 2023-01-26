34904bd8466366e8b84221c9f1f0bc1eee80ac57
Nycollas <sobolevski.academy@gmail.com>
2023-01-26 09:47:24 -0300

    sla

--- routes.js
+++ routes.js
@@ -9,19 +9,11 @@ const porarm = require('./src/controllers/porarm');
 // Iniciando as rotas
 route.get('/', home.pagInicialGet);
 
-<<<<<<< HEAD
-// // Retirar por armário
-// route.get('/armarios/:EDV', porarm.armarios);
-// route.get('/gavetas/:EDV/:armario', porarm.gavetas);
-// route.get('/compartimentos/:EDV/:gaveta', porarm.compartimentos);
-// route.get('/pa-ferramentas/:EDV/:compartimento', porarm.ferramentas);
-=======
 //Retirar por armário
 route.get('/armarios/:EDV', porarm.armarios);
 route.get('/gavetas/:EDV/:armario', porarm.gavetas);
 route.get('/compartimentos/:EDV/:gaveta', porarm.compartimentos);
 route.get('/pa-ferramentas/:EDV/:compartimento', porarm.ferramentas);
->>>>>>> 1abbef0342e7c6b64ef7921a02175f056d7f5804
 
 // route.get('/tipo/:EDV', porfer.tipo);
 // route.get('/subtipo/:EDV/:tipo', porfer.subtipo);
--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,5 +1,15 @@
 module.exports = {
     async pagInicialGet(req, res) {
-        res.render('../views/index');
-    }
+        res.render('../views/index', {retirar:false, devolver:false});
+    },
+    async pagInicialPost(req, res){
+        
+    },
+    
+    async openDevolver(req, res){
+        res.render('../views/index', {retirar:false, devolver:true});
+    },
+    async openRetirar(req, res){
+        res.render('../views/index', {retirar:true, devolver:false});
+    },
 }
