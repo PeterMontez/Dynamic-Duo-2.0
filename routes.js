d145cab18daae23cef00d0b076eeb0d8ff23c771
Nycollas <sobolevski.academy@gmail.com>
2023-01-26 08:30:18 -0300

    UPDATE: back and server started

--- routes.js
+++ routes.js
@@ -8,19 +8,19 @@ const home = require('./src/controllers/home');
 // Iniciando as rotas
 route.get('/', home.pagInicialGet);
 
-// Retirar por armário
-route.get('/armarios/:EDV', porarm.armarios);
-route.get('/gavetas/:EDV/:armario', porarm.gavetas);
-route.get('/compartimentos/:EDV/:gaveta', porarm.compartimentos);
-route.get('/pa-ferramentas/:EDV/:compartimento', porarm.ferramentas);
+// // Retirar por armário
+// route.get('/armarios/:EDV', porarm.armarios);
+// route.get('/gavetas/:EDV/:armario', porarm.gavetas);
+// route.get('/compartimentos/:EDV/:gaveta', porarm.compartimentos);
+// route.get('/pa-ferramentas/:EDV/:compartimento', porarm.ferramentas);
 
-route.get('/tipo/:EDV', porfer.tipo);
-route.get('/subtipo/:EDV/:tipo', porfer.subtipo);
-route.get('/pf-ferramentas/:EDV/:tipo/:subtipo', porfer.ferramentas);
+// route.get('/tipo/:EDV', porfer.tipo);
+// route.get('/subtipo/:EDV/:tipo', porfer.subtipo);
+// route.get('/pf-ferramentas/:EDV/:tipo/:subtipo', porfer.ferramentas);
 
-route.get('/devolver/:EDV', mover.exibir);
-route.post('/devolver/:EDV/:ferramenta', mover.devolver);
-route.post('/retirar/:EDV/:ferramenta', mover.retirar);
+// route.get('/devolver/:EDV', mover.exibir);
+// route.post('/devolver/:EDV/:ferramenta', mover.devolver);
+// route.post('/retirar/:EDV/:ferramenta', mover.retirar);
 
 
 module.exports = route;
