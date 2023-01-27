f16a8426b1742ab2322639261afe0994e3b01872
Peter <peterson.montez9@gmail.com>
2023-01-27 09:08:23 -0300

    home.js retorna listas para o front

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,3 +1,10 @@
+const ferramenta = require('../model/ferramenta');
+const tipo = require('../model/tipo');
+const subtipo = require('../model/subtipo');
+const armario = require('../model/armario');
+const gaveta = require('../model/gaveta');
+const compartimento = require('../model/compartimento');
+
 module.exports = {
     async pagInicialGet(req, res) {
         res.render('../views/index', {retirar:false, devolver:false, cadastrar:false});
@@ -6,7 +13,37 @@ module.exports = {
         
     },
     async openCadastro(req, res){
-        res.render('../views/index',{ retirar:false, devolver:false, cadastrar:true})
+        const ferramentas = await ferramenta.findAll({
+            raw: true,
+            attributes: ['IDFerramenta', 'IDENTIFICACAO', 'OBS', 'STATUS', 'EDV']
+        });
+
+        const tipos = await tipo.findAll({
+            raw: true,
+            attributes: ['IDTipo', 'IDENTIFICACAO']
+        });
+
+        const subtipos = await subtipo.findAll({
+            raw: true,
+            attributes: ['IDSubipo', 'IDENTIFICACAO', 'IDTipo']
+        });
+
+        const armarios = await armario.findAll({
+            raw: true,
+            attributes: ['IDArmario', 'IDENTIFICACAO', 'TIPO', 'DIVISOES']
+        });
+
+        const gavetas = await gaveta.findAll({
+            raw: true,
+            attributes: ['IDGaveta', 'IDENTIFICACAO', 'CONTEUDO', 'IDArmario']
+        });
+
+        const compartimentos = await compartimento.findAll({
+            raw: true,
+            attributes: ['IDCompartimento', 'IDENTIFICACAO', 'IDGaveta']
+        });
+
+        res.render('../views/index', {retirar:false, devolver:false, cadastrar:true, tipos, subtipos, armarios, gavetas, compartimentos, ferramentas})
     },
     
     async openDevolver(req, res){
