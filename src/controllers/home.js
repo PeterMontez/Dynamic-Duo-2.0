8a0735ae3c67a8dfea0ead883bb17f282a9e25b0
Nycollas <sobolevski.academy@gmail.com>
2023-02-01 11:51:09 -0300

    UPDATE: Todas as entradas podem ser inderidas por EDV

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -8,84 +8,161 @@ const compartimento = require('../model/compartimento');
 
 module.exports = {
     async pagInicialGet(req, res) {
-        res.render('../views/index', {retirar:false, devolver:false, cadastrar:false, 
-            retirarEdv:false, devolverEdv:false, cadastrarEdv:false,mensage:''});
+        res.render('../views/index', {
+            retirar: false, devolver: false, cadastrar: false,
+            retirarEdv: false, devolverEdv: false, cadastrarEdv: false, mensage: ''
+        });
     },
-    async pagCadastroGet(req, res){
-        
+    async pagCadastroGet(req, res) {
+
+        const EDV = req.body.edv;
         const cartao = req.body.cartao;
-        console.log(cartao);
 
         const colaboradores = await colaborador.findAll({
             raw: true,
-            attributes: ['EDV','IDENTIFICACAO','CARTAO','ADMIN']
+            attributes: ['EDV', 'IDENTIFICACAO', 'CARTAO', 'ADMIN']
         });
-        const pessoa = await colaborador.findAll({
-            raw: true,
-            attributes: ['EDV','IDENTIFICACAO','CARTAO','ADMIN'],
-            where: {CARTAO: cartao}
-        });
+        PorCartao: {
+            if (cartao != '') {
+                const pessoa = await colaborador.findAll({
+                    raw: true,
+                    attributes: ['EDV', 'IDENTIFICACAO', 'CARTAO', 'ADMIN'],
+                    where: { CARTAO: cartao }
+                });
 
-        const ferramentas = await ferramenta.findAll({
-            raw: true,
-            attributes: ['IDFerramenta', 'IDENTIFICACAO', 'DESCRICAO', 'STATUS', 'EDV']
-        });
+                if (pessoa[0] == undefined) {
+                    if (pessoa[0] == undefined) {
+                        res.render('../views/index', {
+                            retirar: false, devolver: false, cadastrar: false,
+                            retirarEdv: false, devolverEdv: false, cadastrarEdv: false, mensage: 'Pessoa nao cadastrada'
+                        });
+                        break PorCartao;
+                    }
+                }
 
-        const tipos = await tipo.findAll({
-            raw: true,
-            attributes: ['IDTipo', 'IDENTIFICACAO']
-        });
+                const ferramentas = await ferramenta.findAll({
+                    raw: true,
+                    attributes: ['IDFerramenta', 'IDENTIFICACAO', 'DESCRICAO', 'STATUS', 'EDV']
+                });
 
-        const subtipos = await subtipo.findAll({
-            raw: true,
-            attributes: ['IDSubtipo', 'IDENTIFICACAO', 'IDTipo']
-        });
+                const tipos = await tipo.findAll({
+                    raw: true,
+                    attributes: ['IDTipo', 'IDENTIFICACAO']
+                });
 
-        const armarios = await armario.findAll({
-            raw: true,
-            attributes: ['IDArmario', 'IDENTIFICACAO', 'TIPO', 'DIVISOES']
-        });
+                const subtipos = await subtipo.findAll({
+                    raw: true,
+                    attributes: ['IDSubtipo', 'IDENTIFICACAO', 'IDTipo']
+                });
 
-        const gavetas = await gaveta.findAll({
-            raw: true,
-            attributes: ['IDGaveta', 'IDENTIFICACAO', 'CONTEUDO', 'IDArmario']
-        });
+                const armarios = await armario.findAll({
+                    raw: true,
+                    attributes: ['IDArmario', 'IDENTIFICACAO', 'TIPO', 'DIVISOES']
+                });
 
-        const compartimentos = await compartimento.findAll({
-            raw: true,
-            attributes: ['IDCompartimento', 'IDENTIFICACAO', 'IDGaveta']
-        });
+                const gavetas = await gaveta.findAll({
+                    raw: true,
+                    attributes: ['IDGaveta', 'IDENTIFICACAO', 'CONTEUDO', 'IDArmario']
+                });
 
-        let edv = pessoa[0].EDV;
+                const compartimentos = await compartimento.findAll({
+                    raw: true,
+                    attributes: ['IDCompartimento', 'IDENTIFICACAO', 'IDGaveta']
+                });
 
-        res.render('../views/cadastro', {edv,colaboradores,cartao, pessoa, tipos, subtipos, armarios, gavetas, compartimentos, ferramentas,
-            SelectedTipo:'', SelectedSubtipo:'', SelectedArmario:'', SelectedGaveta:'',SelectedCompartimento:'',
-            SelectedIdentificacao:'',
-            SelectedDescricao:'',
-            SelectedStatus:'', send:false})
+                let edv = pessoa[0].EDV;
+
+                res.render('../views/cadastro', {
+                    edv, colaboradores, cartao, pessoa, tipos, subtipos, armarios, gavetas, compartimentos, ferramentas,
+                    SelectedTipo: '', SelectedSubtipo: '', SelectedArmario: '', SelectedGaveta: '', SelectedCompartimento: '',
+                    SelectedIdentificacao: '',
+                    SelectedDescricao: '',
+                    SelectedStatus: '', send: false
+                })
+            }
+        }
+        PorEdv: {
+            if (EDV != '') {
+                const pessoa = await colaborador.findAll({
+                    raw: true,
+                    attributes: ['EDV', 'IDENTIFICACAO', 'CARTAO', 'ADMIN'],
+                    where: { EDV: EDV }
+                });
+
+                if (pessoa[0] == undefined) {
+                    if (pessoa[0] == undefined) {
+                        res.render('../views/index', {
+                            retirar: false, devolver: false, cadastrar: false,
+                            retirarEdv: false, devolverEdv: false, cadastrarEdv: false, mensage: 'Pessoa nao cadastrada'
+                        });
+                        break PorEdv;
+                    }
+                }
+
+                const ferramentas = await ferramenta.findAll({
+                    raw: true,
+                    attributes: ['IDFerramenta', 'IDENTIFICACAO', 'DESCRICAO', 'STATUS', 'EDV']
+                });
+
+                const tipos = await tipo.findAll({
+                    raw: true,
+                    attributes: ['IDTipo', 'IDENTIFICACAO']
+                });
+
+                const subtipos = await subtipo.findAll({
+                    raw: true,
+                    attributes: ['IDSubtipo', 'IDENTIFICACAO', 'IDTipo']
+                });
+
+                const armarios = await armario.findAll({
+                    raw: true,
+                    attributes: ['IDArmario', 'IDENTIFICACAO', 'TIPO', 'DIVISOES']
+                });
+
+                const gavetas = await gaveta.findAll({
+                    raw: true,
+                    attributes: ['IDGaveta', 'IDENTIFICACAO', 'CONTEUDO', 'IDArmario']
+                });
+
+                const compartimentos = await compartimento.findAll({
+                    raw: true,
+                    attributes: ['IDCompartimento', 'IDENTIFICACAO', 'IDGaveta']
+                });
+
+                let edv = pessoa[0].EDV;
+
+                res.render('../views/cadastro', {
+                    edv, colaboradores, cartao, pessoa, tipos, subtipos, armarios, gavetas, compartimentos, ferramentas,
+                    SelectedTipo: '', SelectedSubtipo: '', SelectedArmario: '', SelectedGaveta: '', SelectedCompartimento: '',
+                    SelectedIdentificacao: '',
+                    SelectedDescricao: '',
+                    SelectedStatus: '', send: false
+                })
+            }
+        }
     },
-    
-    async openRetirar(req, res){
-        res.render('../views/index', {retirar:true, devolver:false, cadastrar:false,retirarEdv:false, devolverEdv:false, cadastrarEdv:false, mensage:''});
-    },
-    
-    async openDevolver(req, res){
-        res.render('../views/index', {retirar:false, devolver:true, cadastrar:false,retirarEdv:false, devolverEdv:false, cadastrarEdv:false, mensage:''});
-    },
-    
-    async openCadastro(req, res){
-        res.render('../views/index', {retirar:false, devolver:false, cadastrar:true,retirarEdv:false, devolverEdv:false, cadastrarEdv:false, mensage:''});
+
+    async openRetirar(req, res) {
+        res.render('../views/index', { retirar: true, devolver: false, cadastrar: false, retirarEdv: false, devolverEdv: false, cadastrarEdv: false, mensage: '' });
     },
 
-    async openRetirarEdv(req, res){
-        res.render('../views/index', {retirar:true, devolver:false, cadastrar:false,retirarEdv:true, devolverEdv:false, cadastrarEdv:false, mensage:''});
+    async openDevolver(req, res) {
+        res.render('../views/index', { retirar: false, devolver: true, cadastrar: false, retirarEdv: false, devolverEdv: false, cadastrarEdv: false, mensage: '' });
     },
-    
-    async openDevolverEdv(req, res){
-        res.render('../views/index', {retirar:false, devolver:true, cadastrar:false,retirarEdv:false, devolverEdv:true, cadastrarEdv:false, mensage:''});
+
+    async openCadastro(req, res) {
+        res.render('../views/index', { retirar: false, devolver: false, cadastrar: true, retirarEdv: false, devolverEdv: false, cadastrarEdv: false, mensage: '' });
     },
-    
-    async openCadastroEdv(req, res){
-        res.render('../views/index', {retirar:false, devolver:false, cadastrar:true,retirarEdv:false, devolverEdv:false, cadastrarEdv:true, mensage:''});
+
+    async openRetirarEdv(req, res) {
+        res.render('../views/index', { retirar: true, devolver: false, cadastrar: false, retirarEdv: true, devolverEdv: false, cadastrarEdv: false, mensage: '' });
+    },
+
+    async openDevolverEdv(req, res) {
+        res.render('../views/index', { retirar: false, devolver: true, cadastrar: false, retirarEdv: false, devolverEdv: true, cadastrarEdv: false, mensage: '' });
+    },
+
+    async openCadastroEdv(req, res) {
+        res.render('../views/index', { retirar: false, devolver: false, cadastrar: true, retirarEdv: false, devolverEdv: false, cadastrarEdv: true, mensage: '' });
     },
 }
